468013695755de4b18069b7884a27cc45722c960
koushik-moorthy <67454365+koushik-moorthy@users.noreply.github.com>
2020-11-07 16:31:54 +0530

    added routes for sent,trash and starred

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MiddlebodyComponent } from './middlebody/middlebody.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MiddlebodyComponent },
+  { path: 'starred', component: MiddlebodyComponent },
+  { path: 'sent', component: MiddlebodyComponent },
+  { path: 'trash', component: MiddlebodyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
