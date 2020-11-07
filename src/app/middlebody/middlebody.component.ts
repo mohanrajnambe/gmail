8313ce46b080ef4424ffb929402c741076837f8c
koushik-moorthy <67454365+koushik-moorthy@users.noreply.github.com>
2020-11-07 13:41:52 +0530

    done with middlebody stylings added for primary,promotions and social

--- src/app/middlebody/middlebody.component.ts
+++ src/app/middlebody/middlebody.component.ts
@@ -8,13 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiddlebodyComponent implements OnInit {
   mails: any;
-  
+  primarymails: any;
+  promotionmails: any;
+  socialmails: any;
+
+  primaryURL: string = '';
+  promotionsURL: string = '';
+  socialURL: string = '';
+  primaryactive: boolean = true;
+  socialactive: boolean = false;
+  promotionsactive: boolean = false;
+
   constructor(private http: HttpClient) { 
     let obs = this.http.get("https://5fa4f5bf732de900162e88cb.mockapi.io/emails/primary");
-    obs.subscribe((response) => this.mails=response);
+    obs.subscribe((response) => {
+      this.primarymails = response;
+      this.mails = this.primarymails;
+    });
+    
+    
+    let obs1 = this.http.get("https://5fa4f5bf732de900162e88cb.mockapi.io/emails/promotions");
+    obs1.subscribe((response) => this.promotionmails = response);
+
+    let obs2 = this.http.get("https://5fa4f5bf732de900162e88cb.mockapi.io/emails/social");
+    obs2.subscribe((response) => this.socialmails = response);
+    // document.querySelector("promotion").style.visibility = none;
   }
 
   ngOnInit(): void {
   }
 
+  getPrimaryMails():void {
+    this.mails = this.primarymails;
+    this.primaryactive = true;
+    this.socialactive = false;
+    this.promotionsactive = false;
+  }
+  getPromotionMails():void {
+    this.mails = this.promotionmails;
+    this.primaryactive = false;
+    this.socialactive = false;
+    this.promotionsactive = true;
+  }
+  getSocialMails():void {
+    this.mails = this.socialmails;
+    this.primaryactive = false;
+    this.socialactive = true;
+    this.promotionsactive = false;
+  }
 }
