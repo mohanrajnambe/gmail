ccc67ee072498954e59de68f7cdd78951540b5da
koushik-moorthy <67454365+koushik-moorthy@users.noreply.github.com>
2020-11-07 15:36:30 +0530

    header deleted from middlebody branch and deleted from appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { MiddlebodyComponent } from './middlebody/middlebody.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     MiddlebodyComponent
   ],
   imports: [
